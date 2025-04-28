38273e819f0740ee19c90e5b4808c8e6f7431a53
Jaironascimentof <jaironascimento718@gmail.com>
2025-04-27 21:48:33 -0300

    Modificações no Banco e Novos Comentários

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
+// Importa a instância do app que foi criada no arquivo 'src/app.js'
 import app from "./src/app.js";
 
+// Define a porta em que o servidor vai escutar as requisições
 const PORT = 3000;
 
+// Inicia o servidor e faz com que ele escute na porta especificada
 app.listen(PORT, () => {
+    // Quando o servidor estiver rodando, exibe uma mensagem no console
     console.log(`Servidor na escuta na porta ${PORT}!`);
 });
