60af5f3d4502fbfa599dadcd661f6d33c3ed58e2
AlainDiego <Alain.passos@outlook.com>
2025-06-15 20:43:10 -0300

    Criação do models/Coluna.js de coluna

---
+++ src/models/Coluna.js
@@ -0,0 +1,22 @@
+import { DataTypes } from 'sequelize';
+import db from '../config/dbConnect.js';
+const Coluna = db.define('Coluna', {
+ id: {
+ type: DataTypes.INTEGER,
+ autoIncrement: true,
+ primaryKey: true
+ },
+ nome: {
+ type: DataTypes.STRING,
+ allowNull: false
+ },
+ ordem: {
+ type: DataTypes.INTEGER,
+ allowNull: false
+ },
+ quadroId: {
+ type: DataTypes.INTEGER,
+ allowNull: false
+ }
+});
+export default Coluna;
