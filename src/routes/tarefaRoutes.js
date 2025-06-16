a86bad9ab5d77c008d594153d02a6882262c4702
LuanGFRicardo <luanfranca.personaluser@gmail.com>
2025-06-15 23:01:48 -0300

    Itens faltantes esquecidos por versionamento

--- src/routes/tarefaRoutes.js
+++ src/routes/tarefaRoutes.js
@@ -3,19 +3,22 @@ import TarefaController from "../controllers/tarefasController.js";
 
 const router = express.Router();
 
+// Criar uma nova tarefa
+router.post("/", TarefaController.createTarefa);
+
 // Listar todas as tarefas
-router.get("/", TarefaController.listarTarefas);
+router.get("/", TarefaController.getAllTarefas);
 
 // Buscar uma tarefa por ID
-router.get("/:id", TarefaController.buscarTarefaPorId);
-
-// Cadastrar uma nova tarefa
-router.post("/", TarefaController.cadastrarTarefa);
+router.get("/:id", TarefaController.getTarefaById);
 
 // Atualizar uma tarefa
-router.put("/:id", TarefaController.atualizarTarefa);
+router.put("/:id", TarefaController.updateTarefa);
 
 // Deletar uma tarefa
-router.delete("/:id", TarefaController.deletarTarefa);
+router.delete("/:id", TarefaController.deleteTarefa);
+
+// Buscar por título (se usar o método de busca por nome)
+router.get("/search/:titulo", TarefaController.searchTarefaByTitulo);
 
 export default router;
