c68b00347af9a525b6659f16a2c8679a06e64bbb
Jaironascimentof <jaironascimento718@gmail.com>
2025-06-15 21:06:54 -0300

    Criação das rotas buscar, atualizar e deletar em routes/colunasRoutes.js

--- src/routes/colunasRoutes.js
+++ src/routes/colunasRoutes.js
@@ -9,4 +9,11 @@ router.post("/", ColunaController.cadastrarColuna);
 router.put("/:id", ColunaController.atualizarColuna);
 router.delete("/:id", ColunaController.deletarColuna);
 
+// Define a rota GET '/' que chama o método de buscar por ID
+router.get("/:id", UsuariosController.buscarUsuarioPorId);
+// Define a rota PUT '/' que chama o método de atualizar um usuário
+router.put("/:id", UsuariosController.atualizarUsuario);
+// Define a rota DELETE '/' que chama o método de deletar um usuário
+router.delete("/:id", UsuariosController.deletarUsuario);
+
 export default router;
