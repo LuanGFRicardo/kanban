2c10b282aa1357d7231437ad158981e30010821c
AlainDiego <Alain.passos@outlook.com>
2025-06-15 21:34:04 -0300

    quadroService.js

---
+++ src/services/quadroService.js
@@ -0,0 +1,48 @@
+import { QuadroDto } from "../dtos/quadroDTO.js";
+import { QuadroRepository } from "../repositories/QuadroRepository.js";
+
+export class QuadroService {
+  constructor() {
+    this.quadroRepository = new QuadroRepository();
+  }
+
+  createQuadro = async (quadroData) => {
+    const quadro = QuadroDto.fromRequest(quadroData);
+    return await this.quadroRepository.create(quadro);
+  };
+
+  getAllQuadros = async () => {
+    return await this.quadroRepository.findAll();
+  };
+
+  getQuadroById = async (id) => {
+    const quadro = await this.quadroRepository.findById(id);
+    if (!quadro) {
+      throw new Error("Quadro não encontrado.");
+    }
+    return quadro;
+  };
+
+  updateQuadro = async (id, quadroData) => {
+    const quadroAtualizado = await this.quadroRepository.update(id, quadroData);
+    if (!quadroAtualizado) {
+      throw new Error("Quadro não encontrado.");
+    }
+    return quadroAtualizado;
+  };
+
+  deleteQuadro = async (id) => {
+    const quadroDeletado = await this.quadroRepository.delete(id);
+    if (!quadroDeletado) {
+      throw new Error("Quadro não encontrado.");
+    }
+    return quadroDeletado;
+  };
+
+  searchQuadroByNome = async (nome) => {
+    if (!nome || nome.trim() === "") {
+      throw new Error("Informe o nome do quadro para busca.");
+    }
+    return await this.quadroRepository.searchByNome(nome);
+  };
+}
