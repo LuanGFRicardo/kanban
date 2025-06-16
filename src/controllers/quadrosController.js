e5ce3e1410a1c0b532ef837bf22ac7b875b964e4
Jaironascimentof <jaironascimento718@gmail.com>
2025-06-15 22:01:22 -0300

    deleteQuadro

--- src/controllers/quadrosController.js
+++ src/controllers/quadrosController.js
@@ -63,17 +63,14 @@ class QuadroController {
     }
   };
 
-  static async deletarQuadro(req, res) {
+  deleteQuadro = async (req, res) => {
     try {
-      const id = req.params.id;
-      const deletou = await Quadro.destroy({ where: { id } });
-      deletou
-        ? res.status(200).json({ message: "Quadro removido com sucesso" })
-        : res.status(404).send("Quadro não encontrado");
+      await this.quadroService.deleteQuadro(req.params.id);
+      res.status(200).json({ message: "Quadro removido com sucesso!" });
     } catch (error) {
-      res.status(500).json({ message: `${error.message} - falha ao remover quadro` });
+      res.status(500).send(error.message);
     }
-  }
+  };
 }
 
 export default QuadroController;
