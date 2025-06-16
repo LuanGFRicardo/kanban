23a16c44d967023dc16294197c156b0b3f625895
NaiDaros <Naiara.daros22@gmail.com>
2025-06-15 22:16:23 -0300

    deleteColuna

--- src/controllers/colunasController.js
+++ src/controllers/colunasController.js
@@ -58,19 +58,14 @@ class ColunaController {
     }
   };
 
-  static async deletarColuna(req, res) {
+  deleteColuna = async (req, res) => {
     try {
-      const id = req.params.id;
-      const deletou = await Coluna.destroy({ where: { id } });
-      if (deletou) {
-        res.status(200).json({ message: "Coluna removida com sucesso" });
-      } else {
-        res.status(404).send("Coluna não encontrada");
-      }
+      await this.colunaService.deleteColuna(req.params.id);
+      res.status(200).json({ message: "Coluna removida com sucesso!" });
     } catch (error) {
-      res.status(500).json({ message: `${error.message} - falha ao remover coluna` });
+      res.status(500).send(error.message);
     }
-  }
+  };
 }
 
 export default ColunaController;
