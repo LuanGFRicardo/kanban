393caf9ab84047d1fd24289e6a82585de8812248
Fernandamaralss <fernanda.amaralss@outlook.com>
2025-06-15 22:43:02 -0300

    Aplicação de rotas em quadrosRoutes

--- src/routes/quadrosRoutes.js
+++ src/routes/quadrosRoutes.js
@@ -1,12 +1,15 @@
 import express from "express";
 import QuadroController from "../controllers/quadrosController.js";
+import authMiddleware from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-router.get("/", QuadroController.listarQuadros);
-router.get("/:id", QuadroController.buscarQuadroPorId);
-router.post("/", QuadroController.cadastrarQuadro);
-router.put("/:id", QuadroController.atualizarQuadro);
-router.delete("/:id", QuadroController.deletarQuadro);
+// Aplica o middleware em todas as rotas
+router.post("/", authMiddleware, QuadroController.createQuadro);
+router.get("/search/:nome", authMiddleware, QuadroController.searchQuadroByNome); // ✅ mover antes
+router.get("/", authMiddleware, QuadroController.getAllQuadros);
+router.get("/:id", authMiddleware, QuadroController.getQuadroById);
+router.put("/:id", authMiddleware, QuadroController.updateQuadro);
+router.delete("/:id", authMiddleware, QuadroController.deleteQuadro);
 
 export default router;
