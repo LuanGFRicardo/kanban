2524ebe4da917ec69b278b2499e98409df026f13
LuanGFRicardo <luanfranca.personaluser@gmail.com>
2025-06-15 21:44:03 -0300

    Deletar Usuário

--- src/controllers/usuariosController.js
+++ src/controllers/usuariosController.js
@@ -51,23 +51,14 @@ class UsuarioController {
     }
   };
 
-  // Método para deletar usuário
-  static async deletarUsuario(req, res) {
+  deleteUsuario = async (req, res) => {
     try {
-      const { id } = req.params;
-      const deletou = await Usuario.destroy({
-        where: { id }
-      });
-
-      if (deletou) {
-        res.status(200).json({ message: "Usuário removido com sucesso" });
-      } else {
-        res.status(404).json({ message: "Usuário não encontrado" });
-      }
+      await this.usuarioService.deleteUsuario(req.params.id);
+      res.status(200).json({ message: "Usuário removido com sucesso!" });
     } catch (error) {
-      res.status(500).json({ message: error.message });
+      res.status(404).send(error.message);
     }
-  }
+  };
 }
 
 // Exporta o controller para ser usado nas rotas
