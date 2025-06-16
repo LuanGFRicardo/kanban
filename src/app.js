504b3c9a2e9de51e3d9a69e8926004e282b71df7
LuanGFRicardo <luanfranca.personaluser@gmail.com>
2025-06-15 21:12:12 -0300

    Atualizar o app.js

--- src/app.js
+++ src/app.js
@@ -1,56 +1,50 @@
 // Importa o middleware CORS para permitir requisições de origens diferentes (Cross-Origin Resource Sharing)
 import cors from "cors"; 
 
-// Importa o Express para criar o servidor
+// Importa o framework Express para criar o servidor
 import express from "express";
 
 // Importa a configuração de conexão com o banco de dados
 import db from "./config/dbConnect.js";
 
-// Importa o dotenv para gerenciar variáveis de ambiente (como porta, banco, etc.)
+// Importa o pacote dotenv para gerenciar variáveis de ambiente (.env)
 import dotenv from "dotenv";
 
-// Importa as rotas relacionadas às tarefas
+// um único ponto de entrada para os models
+import "./models/index.js"; // simples, limpo e modular
+
+// Importa o arquivo de rotas de tarefas
 import tarefaRoutes from "./routes/tarefaRoutes.js";
-
 import quadroRoutes from "./routes/quadrosRoutes.js";
-
-// Importa as rotas relacionadas aos usuários
+import colunaRoutes from "./routes/colunasRoutes.js";
 import usuarioRoutes from "./routes/usuarioRoutes.js";
 
-// um único ponto de entrada para os models
-import "./models/index.js";
-
-// Carrega as variáveis do arquivo .env
+// Carrega as variáveis de ambiente do arquivo .env
 dotenv.config();
 
-// Cria a aplicação Express
+// Cria uma instância do Express
 const app = express();
 
-// Configura o servidor para aceitar requisições JSON
+// Configura o servidor para aceitar requisições com JSON no corpo
 app.use(express.json());
 
 // Aplica o middleware CORS para permitir que o servidor aceite requisições de diferentes origens
 app.use(cors());
 
-// Conecta ao banco de dados e sincroniza os modelos com as tabelas
+// Sincroniza o banco de dados com os modelos definidos
 db.sync()
   .then(() => {
-    console.log("Banco de dados conectado e sincronizado com sucesso!"); // Se a conexão for bem-sucedida
+    console.log("Banco de dados conectado e sincronizado com sucesso!"); // Se a conexão der certo
   })
   .catch((error) => {
-    console.error("Erro ao conectar no banco de dados:", error); // Se der erro na conexão
+    console.error("Erro ao conectar no banco de dados:", error); // Se a conexão falhar
   });
 
-// Define as rotas para usuários, utilizando o prefixo /api/usuarios
-app.use("/api/usuarios", usuarioRoutes);
-
-// Define as rotas para tarefas, utilizando o prefixo /api/tarefas
+// Define o prefixo '/api/...' para as rotas de tarefas
 app.use("/api/tarefas", tarefaRoutes);
-
-app.use("/api/quadros", quadroRoutes);
-
 app.use("/api/colunas", colunaRoutes);
+app.use("/api/quadros", quadroRoutes);
+app.use("/api/usuarios", usuarioRoutes); 
 
-// Exporta a instância do app para ser usada no servidor principal (server.js)
+// Exporta o app para ser utilizado em outro arquivo (por exemplo, no servidor principal)
 export default app;
