d7ad84e4553eb49ceb19d6c0541bf6454c3b1d53
LuanGFRicardo <luanfranca.personaluser@gmail.com>
2025-06-15 21:44:28 -0300

    Busca por Nome

--- src/controllers/usuariosController.js
+++ src/controllers/usuariosController.js
@@ -59,6 +59,22 @@ class UsuarioController {
       res.status(404).send(error.message);
     }
   };
+
+  searchUsuarioByNome = async (req, res) => {
+    try {
+      const { nome } = req.params;
+      const usuarios = await this.usuarioService.searchUsuariosByNome(nome);
+      if (usuarios.length === 0) {
+        return res.status(404).json({
+          message: "Nenhum usuário encontrado com o nome informado.",
+          nome,
+        });
+      }
+      res.status(200).json(usuarios.map(u => new UsuarioDto(u)));
+    } catch (error) {
+      res.status(500).send(error.message);
+    }
+  };
 }
 
 // Exporta o controller para ser usado nas rotas
